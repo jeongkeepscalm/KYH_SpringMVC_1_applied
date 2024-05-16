f7ed269767842889df4e882662853404ea332f75
오정길 <jeongkeepscalm@gmail.com>
2024-05-16 16:57:40 +0900

    first commit

---
+++ src/main/java/hello/springmvc_1_applied/Springmvc1AppliedApplication.java
@@ -0,0 +1,13 @@
+package hello.springmvc_1_applied;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmvc1AppliedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springmvc1AppliedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springmvc_1_applied/Springmvc1AppliedApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springmvc_1_applied;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springmvc1AppliedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
